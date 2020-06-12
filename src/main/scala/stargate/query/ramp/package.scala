7d2d3866162b90b973ed8c02449af7d627f34ee1
Steve Donnelly <steve.donnelly@datastax.com>
2020-06-12 15:12:54 -0400

    adding function to get last valid entity state before a given transaction id

--- src/main/scala/stargate/query/ramp/package.scala
+++ src/main/scala/stargate/query/ramp/package.scala
@@ -2,7 +2,12 @@ package stargate.query
 
 import java.util.UUID
 
-import scala.concurrent.Future
+import com.datastax.oss.driver.api.core.CqlSession
+import stargate.model.{OutputModel, ScalarCondition}
+import stargate.query
+import stargate.util.AsyncList
+
+import scala.concurrent.{ExecutionContext, Future}
 
 package object ramp {
 
@@ -21,7 +26,17 @@ package object ramp {
   }
 
   type GetTransactionState = UUID => Future[TransactionState.Value]
-  type SetTransactionState = (UUID, TransactionState.Value) => Future[Boolean]
+  type SetTransactionState = (UUID, TransactionState.Value) => Future[Unit]
 
+  case class Context(model: OutputModel, getState: GetTransactionState, setState: SetTransactionState, session: CqlSession,executor: ExecutionContext) {
+    val queryContext = query.Context(model, session, executor)
+  }
+
+  def matchEntities(context: Context, transactionId: UUID, rootEntityName: String, relationPath: List[String], conditions: List[ScalarCondition[Object]]): AsyncList[Map[String,Object]] = {
+    val potentialIds = query.matchEntities(context.queryContext, rootEntityName, relationPath, conditions).dedupe(context.executor)
+    val entities = potentialIds.map(id => {
+
+    })
+  }
 
 }
--- src/main/scala/stargate/query/ramp/read.scala
+++ src/main/scala/stargate/query/ramp/read.scala
@@ -1,5 +1,40 @@
 package stargate.query.ramp
 
+import java.util.UUID
+
+import stargate.{cassandra, query, schema}
+
+import scala.concurrent.Future
+
 object read {
 
+  def entityIdToObjectStates(context: query.Context, entityName: String, id: UUID): Future[List[Map[String,Object]]] = {
+    val baseTable = context.model.baseTables(entityName)
+    val select = query.read.selectStatement(baseTable, Map((schema.ENTITY_ID_COLUMN_NAME, id)))
+    cassandra.queryAsync(context.session, select.build, context.executor).map(cassandra.rowToMap, context.executor).toList(context.executor)
+  }
+
+  def entityIdToLastValidState(context: Context, before: UUID, entityName: String, id: UUID): Future[Option[Map[String,Object]]] = {
+    val states = entityIdToObjectStates(context.queryContext, entityName, id)
+    states.flatMap(states => {
+      val beforeStates = states.reverse.dropWhile(_(schema.TRANSACTION_ID_COLUMN_NAME).asInstanceOf[UUID].compareTo(before) < 0)
+      def isHeadValid(states: List[Map[String,Object]]): Future[Option[Map[String,Object]]] = {
+        states.headOption.map(head => {
+          val isValid = context.getState(head(schema.TRANSACTION_ID_COLUMN_NAME)).map(_ == TransactionState.SUCCESS)(context.executor)
+          isValid.flatMap(isValid => {
+            if(isValid) {
+              if(head.get(schema.TRANSACTION_DELETED_COLUMN_NAME).map(_.asInstanceOf[java.lang.Boolean]).getOrElse(java.lang.Boolean.FALSE)) {
+                Future.successful(Some(head))
+              } else {
+                Future.successful(None)
+              }
+            } else {
+              isHeadValid(states.tail)
+            }
+          })(context.executor)
+        }).getOrElse(Future.successful(None))
+      }
+      isHeadValid(beforeStates)
+    })(context.executor)
+  }
 }
